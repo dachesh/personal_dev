8c6b6294547937d0b6de705c949b7fbea8df7354
Eshelman <eshelman@u082e5f127474543c31f2.ant.amazon.com>
2015-06-15 09:49:16 -0700

    corrected notes

--- pandas_load.py
+++ pandas_load.py
@@ -3,7 +3,7 @@ import numpy as np
 import scipy as sp
 import matplotlib as mpl
 
-url = "http://ipsdatasciencemetrics-1a-i-3fb2431f.us-east-1.amazon.com:1080/ips_tt_metrics_data"
+url = "<file location>"
 ttData = pd.read_csv(url, header = 0, sep="\t")
 
 print (ttData.head())
